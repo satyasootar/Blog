64009d10d6979fffe6357a0640af57e403a30b9e
satyasootar <satya.sootar06@gmail.com>
2025-02-24 23:25:52 +0530

    config added

---
+++ src/appwrite/config.js
@@ -0,0 +1,138 @@
+import conf from '../conf/conf.js'
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+
+export class Service {
+    client = new Client()
+    databases;
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.APPWRITE_URL)
+            .setProject(conf.PROJECT_ID)
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client)
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                conf.DATABASE_ID,
+                conf.COLLECTION_ID,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            console.log("Creating Post: ", error);
+
+        }
+    }
+
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.databases.updateDocument(
+                conf.DATABASE_ID,
+                conf.COLLECTION_ID,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+        } catch (error) {
+            console.log("Failed to update: ", error);
+
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                conf.DATABASE_ID,
+                conf.COLLECTION_ID,
+                slug,
+            )
+            return true
+        } catch (error) {
+            console.log("Failed to delete: ", error);
+            return false
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            await this.databases.getDocument(
+                conf.DATABASE_ID,
+                conf.COLLECTION_ID,
+                slug,
+            )
+            return true
+        } catch (error) {
+            console.log("Get post Failed: ", error);
+            return false
+        }
+    }
+
+    async getAllPost(query = [Query.equal("status", "active")]) {
+        try {
+            await this.databases.listDocuments(
+                conf.DATABASE_ID,
+                conf.COLLECTION_ID,
+                query
+            )
+            return true
+        } catch (error) {
+            console.log("Get all post Failed: ", error);
+            return false
+        }
+    }
+
+    async uploadFile(file) {
+        try {
+            await this.bucket.createFile(
+                conf.BUCKET_ID,
+                ID.unique(),
+                file
+            )
+            return true
+        } catch (err) {
+            console.log("File uploading error: ", err);
+            return false
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFile(
+                conf.BUCKET_ID,
+                fileId,
+            )
+            return true
+        } catch (err) {
+            console.log("File deleting error: ", err);
+            return false
+        }
+    }
+
+    getFilePreview(fileId) {
+        return this.bucket.getFilePreview(
+            conf.BUCKET_ID,
+            fileId
+        )
+    }
+
+
+}
+
+const service = new Service()
+
+export default service;
